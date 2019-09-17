dd6bcd8efbd8a465051746a336c28cf3cec2a57c
Michael DHaem <mikedhaem@me.com>
2019-09-17 08:25:56 -0700

    Convert to MVC Design pattern

---
+++ Todoey/DataModel/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Michael DHaem on 9/14/19.
+//  Copyright © 2019 Michael DHaem. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
